604a5230a3b405a87c03fd546384220d68b7961b
hridoy667 <hridoy15-14667@diu.edu.bd>
2025-03-09 23:42:17 +0600

    some components added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,6 +6,9 @@ import jsLogo from '../assets/javascript.png';
 import tailwindLogo from '../assets/tailwind.png';
 import gitLogo from '../assets/git.png';
 import nodeLogo from '../assets/node.png';
+import pythonLogo from '../assets/python.png';
+import djangoLogo from '../assets/django.png';
+import bootstrapLogo from '../assets/bootstrap.png';
 
 
 export default function Skills() {
@@ -16,24 +19,26 @@ export default function Skills() {
     { id: 4, name: 'JavaScript', img: jsLogo },
     { id: 5, name: 'Tailwind CSS', img: tailwindLogo },
     { id: 6, name: 'Git', img: gitLogo },
-    { id: 7, name: 'Node.js', img: nodeLogo }
+    { id: 7, name: 'Node.js', img: nodeLogo },
+    { id: 8, name: 'python', img: pythonLogo },
+    { id: 9, name: 'django', img: djangoLogo },
+    { id: 10, name: 'bootstrap', img: bootstrapLogo }
   ];
 
   return (
-    <main id="skills" className="py-10 px-4 flex flex-col items-center">
+    <main id="skills" className="py-10 px-3 flex flex-col items-center">
   {/* Section Title */}
   <div className="text-center">
-    <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">My Skills</h2>
-    <p className="text-gray-300 md:text-lg mb-6">Technologies I have experience with.</p>
+    <p className="text-slate-900 md:text-lg mb-6">Technologies I have experience with.</p>
   </div>
 
   {/* Skills Row */}
-  <div className="flex flex-wrap justify-center gap-4 md:gap-6 lg:gap-8 max-w-7xl">
+  <div className="flex flex-wrap justify-center gap-9 md:gap-10 lg:gap-12 max-w-9xl">
     {skills.map((skill) => (
       <div key={skill.id} className="flex flex-col items-center">
         <img
           src={skill.img}
-          alt={skill.name}
+          alt={skill.name} 
           className="w-12 h-12 md:w-16 md:h-16 transition-transform hover:scale-110"
         />
       </div>
